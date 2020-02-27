585a6786bdd81bc762f148a648f9c06cea34edea
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-27 17:34:34 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import Input from 'components/Input/Input';
-import Button from 'components/Button/Button';
-import BookList from 'components/BookList/BookList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from 'pages/HomePage';
+import BookPage from 'pages/BookPage';
 
 function App() {
-  const books = [
-    { id: 1, bookTitle: '123', author: 'authors' },
-    { id: 2, bookTitle: '123', author: 'authors' },
-    { id: 3, bookTitle: '123', author: 'authors' },
-    { id: 4, bookTitle: '123', author: 'authors' },
-    { id: 5, bookTitle: '123', author: 'authors' },
-    { id: 6, bookTitle: '123', author: 'authors' },
-    { id: 7, bookTitle: '123', author: 'authors' },
-    { id: 8, bookTitle: '123', author: 'authors' },
-    { id: 9, bookTitle: '123', author: 'authors' }
-  ];
-
   return (
-    <div>
-      <Input />
-      <Button>Btn</Button>
-
-      <BookList books={books} />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/items/:itemId">
+          <BookPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
