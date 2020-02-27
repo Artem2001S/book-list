e61cf2b87a650efc20527f5adc9a695359190dee
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-27 17:37:22 +0300

    add 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 import BookPage from 'pages/BookPage';
+import NotFoundPage from 'pages/NotFoundPage';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route path="/items/:itemId">
           <BookPage />
+        </Route>
+        <Route path="/">
+          <NotFoundPage />
         </Route>
       </Switch>
     </Router>
