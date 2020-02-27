1ff81fbed7328d9322e216c4d0ca782bc1808f3c
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-02-27 17:11:52 +0300

    add BookList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Input from 'components/Input/Input';
 import Button from 'components/Button/Button';
-import BookCard from 'components/BookCard/BookCard';
+import BookList from 'components/BookList/BookList';
 
 function App() {
+  const books = [
+    { id: 1, bookTitle: '123', author: 'authors' },
+    { id: 2, bookTitle: '123', author: 'authors' },
+    { id: 3, bookTitle: '123', author: 'authors' },
+    { id: 4, bookTitle: '123', author: 'authors' },
+    { id: 5, bookTitle: '123', author: 'authors' },
+    { id: 6, bookTitle: '123', author: 'authors' },
+    { id: 7, bookTitle: '123', author: 'authors' },
+    { id: 8, bookTitle: '123', author: 'authors' },
+    { id: 9, bookTitle: '123', author: 'authors' }
+  ];
+
   return (
     <div>
       <Input />
       <Button>Btn</Button>
-      <BookCard book={{ id: 1, bookTitle: '123', author: 'authors' }} />
+
+      <BookList books={books} />
     </div>
   );
 }
---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BookCard from 'components/BookCard/BookCard';
+import classes from './BookList.module.scss';
+
+export default function BookList({ books }) {
+  return (
+    <div className={classes.BookList}>
+      {books.map(book => (
+        <BookCard key={book.id} book={book} />
+      ))}
+    </div>
+  );
+}
+
+BookList.propTypes = {
+  books: PropTypes.array.isRequired
+};
