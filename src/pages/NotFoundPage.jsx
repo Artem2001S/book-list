c96afb1549f57afdb425892541cae5189008580e
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-03-02 21:29:02 +0300

    change NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <>
+      <h1 style={{ textAlign: 'center' }}>404</h1>
+      <h3 style={{ textAlign: 'center' }}>
+        Go to <Link to="/">Home</Link>
+      </h3>
+    </>
   );
 }
